99f4e1562f43de1917278122f34ae7d392264c6b
Eber Brea <88146419+ebrea@users.noreply.github.com>
2022-03-09 14:37:04 -0300

    Add files via upload

---
+++ validarSenha.js
@@ -0,0 +1,51 @@
+
+// var s1 = 'd100@abc';
+// testarSenha(s1)
+
+
+function testarSenha(senha) {
+
+    var validos = '1234567890ABCDEFGHIJKLMNOPQRSTUVXYZW()#@$%&*-_+=';
+    var alfabetoMaiusculo = 'ABCDEFGHIJKLMNOPQRSTUVXYZW';
+
+    var lista = senha.split('');
+    var caracter = false;
+    var maiuscula = 0;
+    var resultado = '';
+
+    if (senha.length < 6 || senha.length > 10) {
+        resultado = 'A senha tem '+pw.length+ ' dígitos (deve ter de 6 a 10)';
+        alert(resultado);
+        return resultado;                                 
+    }  
+
+    if (!isNaN(lista[0])) {
+        resultado = 'Não pode começar por número';
+        alert(resultado);
+        return resultado;
+    }
+
+    for (i=0; i<lista.length; i++) {   // verificando dígito a dígito
+        var letraM = lista[i].toUpperCase();
+        caracter = validos.includes(letraM); 
+
+        if (! caracter) {  // checa se o caracter é válido
+            resultado = 'Caracter inválido detectado'; 
+            alert(resultado);      
+            return resultado;
+        } 
+        if (alfabetoMaiusculo.includes(lista[i])) { 
+            maiuscula = 1;  // verifica se tem pelo menos uma letra maiúscula
+        }
+    }
+    if (caracter) {  // sendo todos os caractéres válidos ...
+        if (maiuscula > 0) {  // vê Se há pelo menos uma letra maiúscula
+            resultado = 'Senha OK';
+            // alert(resultado);
+        } else {
+            resultado = 'Deve haver pelo menos uma letra maiúscula'; 
+            alert(resultado);           
+        }
+        return resultado;       
+    }
+}
